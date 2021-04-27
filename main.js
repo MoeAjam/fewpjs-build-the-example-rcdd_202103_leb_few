92447d45acd0f79c687669f621dc3bf07179b1f7
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-27 19:20:15 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ like.forEach(item => item.addEventListener('click', function(e) {
       //modalMessage.classList.remove('hidden');
       let timeoutID = window.setTimeout(modalMessage.classList.add('hidden'),3000);
 
-    }))
-  })
+    })
+  }))
 }
 
 
