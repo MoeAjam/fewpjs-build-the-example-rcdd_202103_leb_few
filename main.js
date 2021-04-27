137d08cd024c538357d141b434a0f9c958fe7750
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-27 19:14:34 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -13,8 +13,9 @@ for (let i=0;i< like.length;i++){
     .catch(()=>{
       error.classList.remove('hidden');
       modalMessage.insertAdjacentHTML('beforeend',`${error.message}`);
-      modalMessage.classList.add('hidden');
+      
       let timeoutID = setTimeout(modalMessage.classList.remove('hidden'),3000);
+      modalMessage.classList.add('hidden');
     })
   })
 }
