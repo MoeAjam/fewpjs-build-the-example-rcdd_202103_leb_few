c0c2c2c7eba3ab59686ed388bfc12632033d9bb2
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-27 19:36:23 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for (let i=0;i<like.length;i++){
   like[i].addEventListener('click',function(e){
     mimicServerCall()
     .then((response)=>{
-      like.innerHTML = "${FULL_HEART}";
+      like.innerHTML = `${FULL_HEART}`;
     })
     .catch(()=>{
       error.classList.remove('hidden');
